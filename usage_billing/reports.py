85cfaafb09725c1d2e5f45e0d067343c955f9ff6
Mwogi <mwogin@gmail.com>
2022-01-18 06:12:15 +0000

    initial

--- usage_billing/reports.py
+++ usage_billing/reports.py
@@ -3,15 +3,28 @@ import datetime
 
 
 @frappe.whitelist(allow_guest=True)
-def get_encounters_for_month(month=datetime.date.today().month):
-    month_start = datetime.date.today().replace(day=1, month=month)
+def get_encounters_for_month(*args, **kwargs):
+    month = datetime.date.today().month # by default use current month
+    year = datetime.date.today().year # by default use current year
+    try:
+        if 'month' in kwargs:month=int(kwargs['month'])
+        if 'year' in kwargs:year=int(kwargs['year'])
+    except:
+        frappe.local.response.update({"error":"invalid month/year provided"})
+        return
+    
+    month_start = datetime.date.today().replace(day=1, month=month, year=year)
     month_end = (month_start.replace(day=28) + datetime.timedelta(days=4)) 
     month_end = month_end - datetime.timedelta(days=month_end.day)
 
-    encounters = frappe.db.get_list('Patient Appointment', 
-        filters=[[
-        'date', 'between', [month_end, month_start]]],
-        fields=['appointment_date', 'patient', 'a ppointment_time', 'name']
-    )
+    encounters = frappe.db.get_list('Patient Encounter', 
+        filters={'encounter_date': ['between', (month_start, month_end)]}, 
+        fields=['encounter_date', 'patient', 'encounter_time', 'name', 'patient_name', 'appointment', 'appointment_type', ])
 
-    return encounters
+    frappe.local.response.update({"encounters":encounters, "from": month_start, "to": month_end, "count": len(encounters)})
+    return
+
+def load_credits(token):
+
+    verified_amount = "" # decode and check jwt sent
+    pass
